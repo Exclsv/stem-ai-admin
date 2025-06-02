f69b3fbe01c10cc34440286f8074667014b39ed6
Exclsv <exkiller3101@gmail.com>
2025-06-02 17:47:18 +0500

    fix: :bug: Fix GET_USER_BY_ACCESSTOKEN_URL

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -3,7 +3,7 @@ import { AuthModel, UserModel } from './_models';
 
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
-export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/me`;
+export const GET_USER_BY_ACCESSTOKEN_URL = `${API_URL}/auth/me`;
 export const LOGIN_URL = `${API_URL}/auth/login/`;
 export const REGISTER_URL = `${API_URL}/auth/register/`;
 export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`;
